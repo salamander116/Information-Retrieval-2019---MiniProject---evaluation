c179ddf35bd99ae6115e703850dc9c2d7cffb825
Miguel Candeias <js.mc.tic@gmail.com>
2019-10-12 18:05:56 +0100

    yaisss more debug
    more debug

--- MainModule.py
+++ MainModule.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
+
 import RetrievalModelsMatrix
 import collectionloaders
 import simpleparser as parser
@@ -15,6 +16,28 @@ def helpz():
     print("q ---> quit")
     print("mm ---> retrieval model matrix")
     print("mii ---> retrieval model inverted index")
+    pass
+
+
+def drawLMJMGraphic(lmjarray):
+    plt.figure()
+    i = 0.1
+    for scorelm in lmjarray:
+        jet = plt.get_cmap('jet')
+        colors = iter(jet(np.linspace(0, 1, 10)))
+        color = next(colors)
+
+        plt.plot(i, scorelm, color='b', alpha=1)
+        plt.gca().set_aspect('equal', adjustable='box')
+        i = i + 0.1
+
+    plt.xlabel('Lambda')
+    plt.ylabel('Precision@10')
+    plt.ylim([0.0, 1.0])
+    plt.xlim([0.0, 1.0])
+    plt.title('LMJM (P@10={0:0.2f})')
+    plt.show()
+    plt.close()
     pass
 
 
@@ -44,7 +67,7 @@ def graphic(precision_vsmArr, recall, map_vsm):
     plt.ylim([0.0, 1.0])
     plt.xlim([0.0, 1.0])
     plt.title('Precision-Recall (MAP={0:0.2f})'.format(map_vsm))
-    plt.savefig('results/prec-try2.png', dpi=100)
+    plt.savefig('results/maplmjm.png', dpi=100)
     pass
 
 
@@ -59,7 +82,33 @@ def inputParser(line):
     return ngrams
 
 
-def vsm(vectorizer, cl, verbose, ):
+def lmjm(vectorizer, cl, verbose, lmbd):
+    corpus = parser.stemCorpus(cl.corpus_cranfield['abstract'])
+    tf_cranfiled = vectorizer.fit_transform(corpus).toarray()
+    models = RetrievalModelsMatrix.RetrievalModelsMatrix(tf_cranfiled, vectorizer)
+    scores_array = []
+    map_lmjm = 0
+    i = 0
+    j = 1
+    for query in cl.queries_cranfield['query']:
+        if i == 0:
+            i += models.score_lmjm(parser.stemSentence(query), lmbd)
+        else:
+            i = i * models.score_lmjm(parser.stemSentence(query), lmbd)
+
+        [average_precision, precision, recall, thresholds] = cl.eval(i, j)
+        map_lmjm = map_lmjm + average_precision
+        scores_array.append(precision)
+        if verbose:
+                plt.plot(recall, precision, color='silver', alpha=0.1)
+                print('qid =', i, 'VSM     AP=', average_precision)
+        i = i + 1
+    map_lmjm = map_lmjm / cl.num_queries
+    finalres = [scores_array, recall, map_lmjm]
+    return finalres
+
+
+def vsm(vectorizer, cl, verbose):
     corpus = parser.stemCorpus(cl.corpus_cranfield['abstract'])
     tf_cranfield = vectorizer.fit_transform(corpus).toarray()
     models = RetrievalModelsMatrix.RetrievalModelsMatrix(tf_cranfield, vectorizer)
@@ -86,23 +135,38 @@ def vsm(vectorizer, cl, verbose, ):
 cl = collectionloaders.CranfieldTestBed()
 user_input = input("Command?")
 verbose = True
+
 while user_input.lower() != "q":
     if user_input.lower() == "mm":
+        user_model_option = input("model : ex. vsm/lmjm/lmd/bm25")
         user_input = input("Number of N-grams? \t ex: 1 2 3")
         result = inputParser(user_input)
         if result == 0:
             vectorizer = CountVectorizer()
-            [precision_vsm, recall, map_vsm] = vsm(vectorizer, cl, verbose)
-            graphic(precision_vsm, recall, map_vsm)
         else:
             vsmArray = []
             for num in result:
-                vectorizer = CountVectorizer(ngram_range=(1, int(num)+1), token_pattern='(?=([a-zA-Z]{' + num + '}))',
-                                             min_df=0, stop_words='english')
+                vectorizer = CountVectorizer(ngram_range=(1, int(num)), token_pattern=r'\b\w+\b',
+                                             min_df=1, stop_words='english')
+
+        if user_model_option.lower() == "vsm":
+            if result == 0:
+                [precision_vsm, recall, map_vsm] = vsm(vectorizer, cl, verbose)
+                graphic(precision_vsm, recall, map_vsm)
+            else:
                 [precision_vsm, recall, map_vsm] = vsm(vectorizer, cl, verbose)
                 vsmArray.append(precision_vsm)
                 graphic(vsmArray, recall, map_vsm)
+        elif user_model_option.lower() == "lmjm":
+            pointsArray = []
+            for x in range(0, 11):
+                lmbd = input("Lambda ----> betwenn 0 and 1")
+                [precision_lmjm, recall, map_lmjm] = lmjm(vectorizer, cl, verbose, lmbd)
+                pointsArray.append(precision_lmjm)
 
+            graphic(precision_lmjm, recall, map_lmjm)
+        elif user_model_option.lower("lmd"):
+            pass
     elif user_input.lower() == "mii":
         model_inverted_index()
     else:
--- RetrievalModelsMatrix.py
+++ RetrievalModelsMatrix.py
@@ -12,39 +12,54 @@ class RetrievalModelsMatrix:
         self.term_coll_freq = np.sum(tf, axis=0)
         self.docLen = np.sum(tf, axis=1)
 
-        self.idf = np.log(np.size(tf, axis = 0) / self.term_doc_freq)
+        self.idf = np.log(np.size(tf, axis=0) / self.term_doc_freq)
         self.tfidf = np.array(tf * self.idf)
 
         self.docNorms = np.sqrt(np.sum(np.power(self.tfidf, 2), axis=1))
 
         ## LMD statistics
 
-
         ## LMJM statistics
 
-        
+        self.features = self.vectorizer.get_feature_names()
+        self.lc = len(self.features)
+        self.d= np.shape(self.tf)[0]
+        print("---->lc:" + str(self.lc))
+        print("---->d:" + str(self.d))
+
+
         ## BM25 statistics
 
-        
     def score_vsm(self, query):
         query_vector = self.vectorizer.transform([query]).toarray()
         query_norm = np.sqrt(np.sum(np.power(query_vector, 2), axis=1))
 
         doc_scores = np.dot(query_vector, self.tfidf.T) / (0.0001 + self.docNorms * query_norm)
-#       doc_scores = np.nan_to_num(doc_scores, 0)
+        #       doc_scores = np.nan_to_num(doc_scores, 0)
 
         return doc_scores
 
     def score_lmd(self, query):
-
         return doc_scores
 
-    def score_lmjm(self, query):
-        return 0
+    def score_lmjm(self, query, lmbdq):
+        queryvector = self.vectorizer.build_tokenizer()
+        for term in queryvector(query):
+            try:
+                index = self.features.index(term)
+                lt = np.sum(self.tf[:, index], axis=0)
+                np.shape(self.tf)
+                for i in range(0, len(self.tf)):
+                    ftd = self.tf[i, index]
+            except:
+                return 0
+            first = float(lmbdq)*(lt/self.lc)
+            second = (1-float(lmbdq))*(ftd/self.d)
+
+        return first + second
 
     def score_bm25(self, query):
         return 0
 
     def scoreRM3(self, query):
         return 0
-
